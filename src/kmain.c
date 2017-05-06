d4148fe26419e604daffba85dd788db3944e125c
Rarioty <arthur.chevalier@inria.fr>
2017-05-07 01:07:03 +0200

    feat(kernel): Adding gdt in makefile and initialize it in the kernel main function

--- src/kmain.c
+++ src/kmain.c
@@ -3,17 +3,45 @@
 #endif
 
 #include <kernel/tty.h>
+#include <kernel/gdt.h>
 #include <stdio.h>
 
-int kernel_main(void)
+int main(void);
+
+void kernel_main(void)
 {
     terminal_initialize();
     terminal_writeline("=== FlowS ===");
 
-    for (int i = 20; i < 70; ++i)
+    // Check if we are in protected mode !
+    unsigned int cr0;
+    asm("movl %%cr0, %0\n" : "=r"(cr0));
+
+    printf("Protected mode: ");
+    if (cr0 & 1)
+        printf("ON\n");
+    else
     {
-        printf("testing scrolling (%c)\n", i);
+        printf("OFF\n");
+        printf("ERROR: We should be in protected mode !\n");
+        return;
     }
+
+    printf("Initializing GDT...\n");
+    init_gdt();
+
+    asm("movw $0x18, %ax \n \
+         movw %ax, %ss   \n \
+         movl $0x20000, %esp");
+
+    main();
+}
+
+int main(void)
+{
+    printf("kernel: GDT loaded !\n");
+
+    while(1);
 
     return 1;
 }
