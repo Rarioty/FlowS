672ed6062c6b58c1d994eb49ddc8721c7590069c
Rarioty <arthur.chevalier@inria.fr>
2017-05-07 01:11:50 +0200

    feat(kernel/io): Adding io functions

---
+++ include/kernel/io.h
@@ -0,0 +1,20 @@
+#ifndef _IO_H_
+#define _IO_H_
+
+#define cli asm("cli"::)
+
+#define sti asm("sti"::)
+
+#define outb(port,value)    \
+    asm volatile ("outb %%al, %%dx" :: "d" (port), "a" (value));
+
+#define outbp(port,value)   \
+    asm volatile ("outb %%al, %%dx; jmp 1f; 1:" :: "d" (port), "a" (value));
+
+#define inb(port) ({                                            \
+    unsigned char _v;                                           \
+    asm volatile ("inb %%dx, %%al" : "=a"(_v) : "d" (port));    \
+    _v;                                                         \
+})
+
+#endif
