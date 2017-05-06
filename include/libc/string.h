f4b3160a5b07d22fdbbf0575c224809360d80619
Rarioty <arthur.chevalier@inria.fr>
2017-05-07 01:05:19 +0200

    feat(libc/string): Adding some memory handle functions

--- include/libc/string.h
+++ include/libc/string.h
@@ -2,6 +2,7 @@
 #define _STRING_H_
 
 #include <stddef.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,6 +11,9 @@ extern "C" {
 #define EOF '\n'
 
 size_t strlen(const char*);
+void* memcpy(void* restrict, const void*, size_t);
+int memcmp(const void*, const void*, size_t);
+void* memset(void*, int, size_t);
 
 #ifdef __cplusplus
 }
