6305267b78712735fa3ed91ef7e931817c0b7435
Rarioty <arthur.chevalier@inria.fr>
2017-05-20 01:32:58 +0200

    fix(travis): Fixing mistake for old gcc

--- src/libc/string/memcpy.c
+++ src/libc/string/memcpy.c
@@ -2,9 +2,11 @@
 
 void* memcpy(void* dstptr, const void* srcptr, size_t size)
 {
+    size_t i;
+
     unsigned char* dst = (unsigned char*) dstptr;
     const unsigned char* src = (const unsigned char*) srcptr;
-    for (size_t i = 0; i < size; ++i)
+    for (i = 0; i < size; ++i)
         dst[i] = src[i];
 
     return dstptr;
